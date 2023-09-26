9908bdde42d52391b5f261549f4089f6baebcab6
thomas.caron <tc.caron91@gmail.com>
2023-09-26 09:20:53 +0200

    Better lighting

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import Ground from './ground/Ground.tsx'
+import { SoftShadows } from '@react-three/drei'
 import { SampleSphere } from './objects/SampleSphere.tsx'
 import CharacterController from './player/CharacterController.tsx'
 
@@ -6,7 +7,16 @@ const Experience = () => {
   return (
     <>
       {/* LIGHTS */}
+      <SoftShadows size={10} samples={20} />
       <ambientLight intensity={1} />
+      <directionalLight color={'#bcbffe'} intensity={0.5} position={[-10, 2, -6]} shadow-mapSize={[1024, 1024]} />
+      <directionalLight
+        castShadow
+        color={'#feefc4'}
+        intensity={2}
+        position={[10, 12, 6]}
+        shadow-mapSize={[1024, 1024]}
+      />
 
       {/* CHARACTER */}
       <CharacterController />
